51ad0ac9ba4f238265ff4a6d217243015f40a81f
calistalu <143591016+calistalu@users.noreply.github.com>
2023-12-01 22:43:08 +0800

    12.1晚

--- js/basic.js
+++ js/basic.js
@@ -6,8 +6,7 @@ window.addEventListener("scroll", function() {
     const translationX = scrolled;  
     const translationY = scrolled;  
     document.getElementById("box2").style.transform = "translate(" + translationX + "px, " + translationY + "px) ";
-    document.getElementById("box1").style.transform = "translateX(-50%) rotate(" + rotationAngle + "deg)";
-
+    
   });
 
   document.addEventListener('DOMContentLoaded', function() {
